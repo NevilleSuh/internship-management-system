25279192d3115cda50fb47b8cbe8d87d1dfd96e1
Ngulefac Terence <telluriumtriol@gmail.com>
2025-06-24 10:46:11 +0100

    final changes

--- Student-Complaint-Franklin/app/Http/Controllers/Student/HomeController.php
+++ Student-Complaint-Franklin/app/Http/Controllers/Student/HomeController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Auth;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
+
 use App\Models\Post;
 use App\Models\Application;
 use App\Models\Category;
